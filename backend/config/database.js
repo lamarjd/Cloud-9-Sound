5d06e796ecffc6c7aeca40342e93f349f8938d91
lamarjd <jacob.lamar16@gmail.com>
2022-12-06 22:11:23 -0800

    fixing render deploy issues

--- backend/config/database.js
+++ backend/config/database.js
@@ -13,14 +13,14 @@ module.exports = {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     seederStorage: 'sequelize',
-    define: {         // define schema here
-      schema: process.env.SCHEMA
-    },
     dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false
       }
+    },
+    define: {         // define schema here
+      schema: process.env.SCHEMA
     }
   }
 };
